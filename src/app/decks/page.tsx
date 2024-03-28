42276fbd979f842315f0a9efe9a8f954fd8985a7
Ross Duris <rossduris@gmail.com>
2024-03-28 14:50:02 -0400

    update ui

--- src/app/decks/page.tsx
+++ src/app/decks/page.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 const Decks = async () => {
   const decks = await getAllDecks();
   return (
-    <div className="flex">
+    <div className="flex text-black gap-2">
       {decks
         ? decks.map((deck) => (
             <Link
               href={`decks/${deck.id}`}
               key={deck.id}
-              className="p-4 border"
+              className="p-4 border bg-white"
             >
               {deck.name}
             </Link>
